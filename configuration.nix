68d7a64d93897e38cb158a9dcf4c7523526843e7
firesquid6 <jdeiss06@gmail.com>
2023-12-28 10:45:13 -0600

    remove hardware config from git

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./local-configuration.nix
       inputs.home-manager.nixosModules.default
     ];
 
