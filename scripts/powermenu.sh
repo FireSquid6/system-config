100ac12bb44da6729bc5dbfef77d8d16c5b24b6c
firesquid6 <jdeiss06@gmail.com>
2024-02-26 19:20:32 -0600

    add bluetooth and powermenu

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -6,13 +6,13 @@ widthpercent=number
 
 typeset -A menu
 menu=(
-    [  Projects]="kitty /home/firesquid/source"
-    [  Lock]="/etc/nixos/scripts/lock.sh"
-    [󰒲  Suspend]="systemctl suspend"
-    [⏻  Shutdown]="shutdown -h now"
-    [󰍃  Logout]="i3-msg exit"
-    [  Wifi Settings]="/etc/nixos/scripts/wifi-menu.sh"
-    [󰌙  Disconnect]="/etc/nixos/scripts/disconnect.sh"
-    [󰌘  Connect]="/etc/nixos/scripts/connect.sh"
+    [1.   Wifi Settings]="/etc/nixos/scripts/wifi-menu.sh"
+    [2. 󰂯  Bluetooth]="/etc/nixos/scripts/rofi-bluetooth.sh"
+    [3.   Lock]="/etc/nixos/scripts/lock.sh"
+    [4. 󰒲  Suspend]="systemctl suspend"
+    [5. ⏻  Shutdown]="shutdown -h now"
+    [6. 󰍃  Logout]="i3-msg exit"
+    [7. 󰌙  Disconnect]="/etc/nixos/scripts/disconnect.sh"
+    [8. 󰌘  Connect]="/etc/nixos/scripts/connect.sh"
 )
 menu_nrows=${#menu[@]}
