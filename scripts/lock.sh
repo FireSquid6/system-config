af3331db2874bd97b8f84f94155f59ba9af0fd3b
firesquid6 <jdeiss06@gmail.com>
2024-02-26 20:14:14 -0600

    use betterlockscreen

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,43 +1,44 @@
 #!/bin/sh
 playerctl pause
+betterlockscreen --lock
 
-BLANK='#00000000'
-CLEAR='#ffffff22'
-DEFAULT='#283457ff'
-TEXT='#c0caf5ff'
-WRONG='#f7768eff'
-VERIFYING='#7aa2f7bb'
-BACKGROUND='#1a1b26cc'
-
-i3lock \
---image /home/firesquid/wallpapers/lock.png \
---fill \
---insidever-color=$BACKGROUND     \
---ringver-color=$VERIFYING   \
-\
---insidewrong-color=$BACKGROUND   \
---ringwrong-color=$WRONG     \
-\
---inside-color=$BACKGROUND        \
---ring-color=$DEFAULT        \
---line-color=$BLANK          \
---separator-color=$DEFAULT   \
-\
---verif-color=$TEXT          \
---wrong-color=$TEXT          \
---time-color=$TEXT           \
---date-color=$TEXT           \
---layout-color=$TEXT         \
---keyhl-color=$WRONG         \
---bshl-color=$WRONG          \
-\
---screen 1                   \
---blur 5                     \
---clock                      \
---indicator                  \
---time-str="%I:%M:%S"        \
---date-str="%A, %Y-%m-%d"       \
-\
---verif-text="..." \
---wrong-text="!" \
---ind-pos="w/4:h/2" \
+# BLANK='#00000000'
+# CLEAR='#ffffff22'
+# DEFAULT='#283457ff'
+# TEXT='#c0caf5ff'
+# WRONG='#f7768eff'
+# VERIFYING='#7aa2f7bb'
+# BACKGROUND='#1a1b26cc'
+#
+# i3lock \
+# --image /home/firesquid/wallpapers/lock.png \
+# --fill \
+# --insidever-color=$BACKGROUND     \
+# --ringver-color=$VERIFYING   \
+# \
+# --insidewrong-color=$BACKGROUND   \
+# --ringwrong-color=$WRONG     \
+# \
+# --inside-color=$BACKGROUND        \
+# --ring-color=$DEFAULT        \
+# --line-color=$BLANK          \
+# --separator-color=$DEFAULT   \
+# \
+# --verif-color=$TEXT          \
+# --wrong-color=$TEXT          \
+# --time-color=$TEXT           \
+# --date-color=$TEXT           \
+# --layout-color=$TEXT         \
+# --keyhl-color=$WRONG         \
+# --bshl-color=$WRONG          \
+# \
+# --screen 1                   \
+# --blur 5                     \
+# --clock                      \
+# --indicator                  \
+# --time-str="%I:%M:%S"        \
+# --date-str="%A, %Y-%m-%d"       \
+# \
+# --verif-text="..." \
+# --wrong-text="!" \
+# --ind-pos="w/4:h/2" \
