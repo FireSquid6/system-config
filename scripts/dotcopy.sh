bca10b84add64dcff4cf21a42ab03e1722a7f89c
firesquid6 <jdeiss06@gmail.com>
2024-02-28 20:33:12 -0600

    add better tmux workflow

--- scripts/dotcopy.sh
+++ scripts/dotcopy.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-
-cp /etc/nixos/dotfiles/bashrc ~/.bashrc
-cp /etc/nixos/dotfiles/dunstrc ~/.config/dunst/dunstrc
-cp /etc/nixos/dotfiles/i3 ~/.config/i3/config
-cp /etc/nixos/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
-cp /etc/nixos/dotfiles/picom.conf ~/.config/picom/picom.conf
-cp /etc/nixos/dotfiles/lazygit.yml ~/.config/lazygit/config.yml
-cp /etc/nixos/dotfiles/maestral.ini ~/.config/maestral/maestral.ini
-cp /etc/nixos/dotfiles/rofi.rasi ~/.config/rofi/config.rasi
-cp /etc/nixos/dotfiles/starship.toml ~/.config/starship.toml
-cp /etc/nixos/dotfiles/polybar.ini ~/.config/polybar/config.ini
-cp /etc/nixos/dotfiles/vimrc ~/.vimrc
-cp /etc/nixos/dotfiles/betterlockscreenrc ~/.config/betterlockscreen/betterlockscreenrc
-cp /etc/nixos/dotfiles/tmux.conf ~/.tmux.conf
-cp /etc/nixos/dotfiles/profile ~/.profile
+cd ~/source/dotcopy/
+go run . build
