4e5ef9671bcb5c4597c139152172cb20c468f664
firesquid6 <jdeiss06@gmail.com>
2024-04-08 21:55:00 -0500

    make ink nvim the primary editor

--- scripts/newsystem.sh
+++ scripts/newsystem.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-git clone https://github.com/firesquid6/astronvim-user-config ~/.config/nvim/lua/user
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/astro-nvim
+git clone https://github.com/firesquid6/astronvim-user-config ~/.config/astro-nvim/lua/user
+git clone https://github.com/firesquid6/ink-nvim ~/.config/ink-nvim
+
 
 
