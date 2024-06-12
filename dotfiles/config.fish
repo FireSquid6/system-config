c5abc0680dad6d7290a9818eee8fafc883e89656
firesquid6 <jdeiss06@gmail.com>
2024-06-12 14:02:45 -0500

    add transient prompt

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,10 +1,15 @@
 if status is-interactive
+  function starship_transient_prompt_func
+    starship module character
+  end
+  starship init fish | source
+  enable_transience
+
   set fish_greeting
 
   export PATH="$PATH:/etc/nixos/scripts"
   export PATH="$PATH:/etc/nixos/bins"
 
-  starship init fish | source
   direnv hook fish | source
 
 
