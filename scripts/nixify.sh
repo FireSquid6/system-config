1bb3dec282149bebdf16ef5a4c205d2e575b2008
firesquid6 <jdeiss06@gmail.com>
2024-07-06 12:55:49 -0500

    add a better envrc script

--- scripts/nixify.sh
+++ scripts/nixify.sh
@@ -1,20 +1,56 @@
 #!/usr/bin/env bash
-touch .envrc
 
-if test -f ./flake.nix; then
-  echo "Found a flake.nix. Using that for the .envrc"
-  echo -e "use flake" >> .envrc
-else
-  echo "No flake.nix found. Using nix-shell for the .envrc"
-  echo -e "use nix" >> .envrc
-fi
 
-if grep -Fxq ".envrc" .gitignore
-then
-  echo ".envrc already in .gitignore"
-else
-  echo ".envrc not in .gitignore. Adding it."
-  echo ".envrc" >> .gitignore
-fi
+TYPE=$1  # flake or shell
+NAME=$2  # name of the flake or shell.nix file
 
-direnv allow 
+function copy_file() {
+  # type should be either flake or shell
+  if [ "$TYPE" != "flake" ] && [ "$TYPE" != "shell" ]; then
+    echo "No type specified. Assuming that this project already has a flake.nix or shell.nix"
+    return 1
+  fi
+
+  # the name file should exist
+  FILEPATH="/etc/nixos/dev$(echo $TYPE)s/$NAME.nix"
+  if [ ! -f $FILEPATH ]; then
+    echo "File $FILEPATH does not exist"
+    return 1
+  fi
+
+  # copy the file to the current directory
+  cp $FILEPATH ./$TYPE.nix
+  echo "Copied $FILEPATH to ./$TYPE.nix"
+}
+
+
+
+
+function init_envrc() {
+  if test -f ./flake.nix; then
+    echo "Found a flake.nix. Using nix flake for the .envrc"
+    touch .envrc
+    echo -e "use flake" >> .envrc
+  elif test -f ./shell.nix; then
+    echo "Found a shell.nix. Using nix-shell for the .envrc"
+    touch .envrc
+    echo -e "use nix" >> .envrc
+  else 
+    echo "No flake.nix or shell.nix found. No envrc to init."
+    exit 1
+  fi
+
+
+  if grep -Fxq ".envrc" .gitignore
+  then
+    echo ".envrc already in .gitignore"
+  else
+    echo ".envrc not in .gitignore. Adding it."
+    echo ".envrc" >> .gitignore
+  fi
+
+  direnv allow 
+}
+
+copy_file
+init_envrc
