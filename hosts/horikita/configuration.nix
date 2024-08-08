d3f8be7e60cccd5934180b573a923d9ca1d905b4
firesquid6 <jdeiss06@gmail.com>
2024-08-08 13:08:03 -0500

    fix battery life

--- hosts/horikita/configuration.nix
+++ hosts/horikita/configuration.nix
@@ -2,4 +2,21 @@
 
 {
   networking.hostName = "horikita";
+
+  # August 8 2024
+  # workaround for some issues in pipewire that eat battery life
+  # future me may want to remove this once it's patched
+  services.pipewire = {
+  enable = true;
+  wireplumber = {
+    enable = true;
+    extraConfig = {
+      "10-disable-camera" = {
+        "wireplumber.profiles" = {
+          main."monitor.libcamera" = "disabled";
+        };
+      };
+    };
+  };
+};
 }
