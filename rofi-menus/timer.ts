0b0dcf25af68a9ae4ad396bfb701022e7c9a8569
firesquid6 <jdeiss06@gmail.com>
2024-09-08 23:07:55 -0500

    add a cancel to the timer

--- rofi-menus/timer.ts
+++ rofi-menus/timer.ts
@@ -2,7 +2,15 @@
 
 import { spawnMenu, type MenuItem } from "."
 
-const menus: MenuItem[] = []
+const menus: MenuItem[] = [{
+  name: "Cancel",
+  action: async () => {
+    const process = Bun.spawn(["/etc/nixos/scripts/timer/start.ts", "00:00"], {
+      stdio: ["ignore", "ignore", "ignore"]
+    })
+    await process.exited
+  }
+}]
 
 
 for (let i = 5; i <= 120; i += 5) {
