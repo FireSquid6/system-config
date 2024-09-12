eeee0855df71f9d8b987f6cb37742bd1d0da63a4
firesquid6 <jdeiss06@gmail.com>
2024-09-11 22:40:58 -0500

    fix tmux startup

--- scripts/tmux-startup.sh
+++ scripts/tmux-startup.sh
@@ -6,7 +6,7 @@ if tmux info &> /dev/null; then
 fi
 
 # workbench contains just a basic session I use for programming projects
-tmux new-session -d -c ~/source -t workbench
+tmux new-session -A -d -c ~/source -t workbench
 
 # automatically put a starting project in nixos
 tmux rename-window -t workbench:1 scratch
@@ -14,12 +14,12 @@ tmux new-window -t workbench -n nixos -c /etc/nixos
 tmux select-window -t workbench:scratch
 
 # monitor just has htop and a blank pane
-tmux new-session -d -t monitor
+tmux new-session -A -d -t monitor
 tmux rename-window -t monitor:1 htop
 tmux send-keys -t monitor:htop htop Enter
 tmux new-window -t monitor -n scratchpad
 tmux select-window -t monitor:htop
 
 # home is a basic session for personal stuff
-tmux new-session -d -c ~ -t home
+tmux new-session -A -d -c ~ -t home
 tmux rename-window -t home:1 scratch
