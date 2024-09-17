634503eebe42ef23839200060a645788b12f39a4
firesquid6 <jdeiss06@gmail.com>
2024-09-16 22:40:08 -0500

    make develop.sh work

--- scripts/open-file.sh
+++ scripts/open-file.sh
@@ -2,8 +2,15 @@
 
 project=$1
 file=$2
+pipe=""
 
 # iterate through the parts of $project
+for part in $(echo "$project" | tr "/" "\n"); do
+  pipe="$HOME/.pipes/$part/socket.pipe"
 
+  if [ -e "$pipe" ]; then
+    break
+  fi
+done
 
 nvim --server "$pipe" --remote-send "<CMD>e $file<CR>"
