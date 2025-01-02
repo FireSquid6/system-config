3f543d41660ef0cab05e89f83e056c75e0273f51
firesquid6 <jdeiss06@gmail.com>
2025-01-02 15:54:24 -0600

    add a useful echo

--- scripts/archive-git-repos.sh
+++ scripts/archive-git-repos.sh
@@ -8,5 +8,6 @@ mkdir ./temp
 bun run index.ts
 
 DATE=$(date "+%Y-%m-%d")
+echo "UPLOADING TO S3..."
 aws s3 cp ./last-archive.zip s3://firesquidpersonal/code-archives/"$DATE".zip
 rm last-archive.zip
