b0ca8915f902647114b6100892fcefda27f11d07
firesquid6 <jdeiss06@gmail.com>
2025-01-05 16:35:22 -0600

    fix rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 CONFIGURATION=$1
-if [ -z $1 ] 
+if [ -z "$1" ] 
 then 
   echo "No configuration specified, using hostname"
   CONFIGURATION=$(hostname)
 fi
 
 echo "Rebuilding configuration for $CONFIGURATION"
-sudo nixos-rebuild switch --flake path:///etc/nixos/#$CONFIGURATION
+sudo nixos-rebuild switch --flake path:///etc/nixos/#"$CONFIGURATION"
