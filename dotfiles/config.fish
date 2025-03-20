b9eedb0a4f20463edf4ed23b3c63116da5ff25db
firesquid6 <jdeiss06@gmail.com>
2025-03-20 15:04:36 -0500

    add bun binary to path

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,19 +1,21 @@
+set fish_greeting
+
 if status is-interactive
   if not set -q SSH_CONNECTION
+
+
     function starship_transient_prompt_func
       starship module character
     end
     starship init fish | source
     enable_transience
-
-  else
-    set fish_greeting
   end
 
   direnv hook fish | source
 
   export PATH="$PATH:/etc/nixos/scripts"
   export PATH="$PATH:/etc/nixos/bins"
+  export PATH="$PATH:/home/firesquid/.bun/bin"
 
   alias e='develop.sh'
   alias ge='develop.sh -l'
