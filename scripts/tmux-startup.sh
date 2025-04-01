c295f25be8abdcd00c447c898b343f5a7ff8cc2c
firesquid6 <jdeiss06@gmail.com>
2025-04-01 14:30:38 -0500

    remove bench tmux session

--- scripts/tmux-startup.sh
+++ scripts/tmux-startup.sh
@@ -5,21 +5,11 @@ if tmux info &> /dev/null; then
   exit 1
 fi
 
-# workbench contains just a basic session I use for programming projects
-tmux new-session -A -d -c ~/source -t workbench
-
-# automatically put a starting project in nixos
-tmux rename-window -t workbench:1 scratch
-tmux new-window -t workbench -n nixos -c /etc/nixos
-tmux select-window -t workbench:scratch
-
-# monitor just has htop and a blank pane
 tmux new-session -A -d -t monitor
 tmux rename-window -t monitor:1 htop
 tmux send-keys -t monitor:htop htop Enter
 tmux new-window -t monitor -n scratchpad
 tmux select-window -t monitor:htop
 
-# home is a basic session for personal stuff
 tmux new-session -A -d -c ~/Notes -t home
 tmux rename-window -t home:1 notes
