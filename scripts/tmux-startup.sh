1dbd388c4a553d574955fd47367c13ee74e30cf1
firesquid6 <jdeiss06@gmail.com>
2025-04-01 15:44:27 -0500

    remove notebook workspace and combine it with home

--- scripts/tmux-startup.sh
+++ scripts/tmux-startup.sh
@@ -9,12 +9,7 @@ tmux new-session -d -t home
 tmux rename-window -t home:1 htop
 tmux send-keys -t home:htop htop Enter
 
-
 tmux new-window -t home -n scratchpad
-tmux new-window -t home -n -c notes
+tmux new-window -t home -n notes -c ~/Notes
 
-
-tmux select-window -t monitor:htop
-
-tmux new-session -d -c ~/Notes -t home
-tmux rename-window -t home:1 notes
+tmux select-window -t home:htop
