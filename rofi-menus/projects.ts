185192c931ac86c5e485814c586344feae884b81
firesquid6 <jdeiss06@gmail.com>
2025-04-03 21:26:20 -0500

    fix projects

--- rofi-menus/projects.ts
+++ rofi-menus/projects.ts
@@ -1,22 +1,25 @@
 #!/usr/bin/env bun
 
-import { spawnMenu, type MenuItem } from "."
-import fs from "fs"
+import { spawnMenu, type MenuItem } from ".";
+import fs from "fs";
+import path from "path";
 
-const dirs = fs.readdirSync("/home/firesquid/source")
+const dirs = fs.readdirSync("/home/firesquid/source");
 
-fs.writeFileSync("/home/firesquid/rofi.txt", dirs.join("\n"))
+fs.writeFileSync("/home/firesquid/rofi.txt", dirs.join("\n"));
 
-const menus: MenuItem[] = []
+const menus: MenuItem[] = [];
 
 for (const dir of dirs) {
   menus.push({
     name: `  ${dir}`,
     action: async () => {
-      const process = Bun.spawn(["$SCRIPTS_DIR/proj.sh", dir], {
-        stdio: ["ignore", "ignore", "ignore"]
-      })
-      await process.exited
+      const filepath = path.join(Bun.env.SCRIPTS_DIR!, "proj.sh");
+      const process = Bun.spawn([filepath, dir], {
+        stdio: ["ignore", "ignore", "ignore"],
+      });
+
+      await process.exited;
     }
   })
 }
